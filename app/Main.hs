c63c8f7dc1e9a19eddb29091d7c1f2f8921d0c60
bhakti <bs266@st-andrews.ac.uk>
2025-05-28 19:46:18 +0100

    bruh

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Main (main) where
 
 import Lib
+import Data.Void (Void)
+import Text.Megaparsec ( (<|>), many, Parsec, MonadParsec(try, eof), parse )
+import Data.Functor (($>))
+import Text.Megaparsec.Char (char, string, lowerChar, upperChar, alphaNumChar, space)
 
-import Text.Megaparsec 
+type List a = [a]
 
 main :: IO ()
 main = someFunc
@@ -17,31 +22,31 @@ data Tm = TmNat Int
         | TmApp Tm Tm 
 
 data Prog = Prog {
-    types :: [TyDecl], 
-    funcs :: [Func], 
+    types :: List TyDecl, 
+    funcs :: List Func, 
     funcMain :: Func
 } 
 
 data Func = Func {
     funName :: String, 
     funRetType :: Ty, 
-    funArgs :: [(String, Ty)], -- should be dependent
-    funBody :: [Stmt],
+    funArgs :: List (String, Ty), -- should be dependent
+    funBody :: List Stmt,
     funReturns :: Stmt
 }
 
 data TyDecl = TyDecl {
     tyDeclName :: String, 
-    tyDeclParams :: [TyParam], 
-    tyDeclConstructors :: [Constructor]
+    tyDeclParams :: List TyParam, 
+    tyDeclConstructors :: List Constructor
 }
 
 data TyParam = TmParam String Ty 
             | TyParam String
 
 data Constructor = Constructor {
-    name :: String, 
-    conArgs :: [(String, Ty)], 
+    conName :: String, 
+    conArgs :: List (String, Ty), 
     conTy :: Ty
 }
 
@@ -49,21 +54,62 @@ data Stmt = Assignment String Stmt
         | Return Tm
         | Switch {
             switchOn :: Tm,
-            cases :: [Case]
+            cases :: List Case
         }
 
 data Case = Case {
     caseOn :: Tm,
-    caseBody :: [Stmt]
+    caseBody :: List Stmt
 }
 
 type Parser = Parsec Void String
 
 tmParse :: String -> Prog
 tmParse str = case parse pProg "" str of 
-        Left _ -> error "todo"
+        Left _ -> undefined 
         Right tm -> tm  
 
 pProg :: Parser Prog
-pProg = error "todo"
+pProg = undefined
+
+pSpaces :: Parser a -> Parser a 
+pSpaces p = space *> p <* space
+
+pSemicolon :: Parser Char
+pSemicolon = char ';'
+
+pTyDecl :: Parser TyDecl
+pTyDecl = undefined
+
+pTyDeclName :: Parser String 
+pTyDeclName = undefined 
+
+pTyParam :: Parser TyParam
+pTyParam = undefined 
+
+pLowerString :: Parser String
+pLowerString = (:) <$> lowerChar <*> many alphaNumChar
+
+pUpperString :: Parser String 
+pUpperString = (:) <$> upperChar <*> many alphaNumChar
+
+pConArgs :: Parser [(String, Ty)]
+pConArgs = undefined 
+
+pTy :: Parser Ty 
+pTy = undefined 
+
+pTyDeclConstructor :: Parser Constructor
+pTyDeclConstructor = do
+    _ <- pSpaces $ string "constructor"
+    conName <- pSpaces $ pUpperString 
+    conArgs <- pSpaces $ pConArgs
+    _ <- pSpaces $ string "of"
+    conTy <- pSpaces $ pTy
+    _ <- pSemicolon
+    pure $ Constructor {
+        conName, conArgs, conTy
+    }
+
+
 
